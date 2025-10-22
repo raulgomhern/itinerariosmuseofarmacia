76b8bf0b9ffa1ccb70c20d87caa42786d59c0d83
Raul Gomez Hernandez <raulgomhernandez@gmail.com>
2025-10-22 04:50:38 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.transparent-btn').addEventListener('click', () => {
+  window.location.href = '../html/segundapantalla.html';
+});
+document.querySelector('.itciclointro-btn').addEventListener('click', () => {
+  window.location.href = '../html/itmedicamento.html';
+});
